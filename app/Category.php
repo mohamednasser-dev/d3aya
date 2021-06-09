2c593d81fd427e004bba5ccbdb185bfa836797e7
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-06-09 13:00:33 +0200

    secode commit

--- app/Category.php
+++ app/Category.php
@@ -11,4 +11,17 @@ class Category extends Model
     public function products() {
         return $this->hasMany('App\Product', 'category_id');
     }
+
+
+    public function Sub_categories() {
+        if(session('lang_api') == 'ar') {
+            return $this->hasMany('App\SubCategory', 'category_id')->select('id', 'title_ar as title','category_id')->where('deleted','0');
+        }else{
+            return $this->hasMany('App\SubCategory', 'category_id')->select('id', 'title_en as title','category_id')->where('deleted','0');
+        }
+    }
+
+    public function Category_ads() {
+            return $this->hasMany('App\Categories_ad', 'cat_id')->select('image', 'cat_id','type','ad_type' ,'content')->where('type','category')->where('deleted','0');
+    }
 }
