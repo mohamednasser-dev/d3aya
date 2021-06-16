8d95fbc12c17c768cabd83648e17591626369a65
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-06-16 13:59:01 +0200

    make fav edit

--- app/Favorite.php
+++ app/Favorite.php
@@ -12,6 +12,8 @@ class Favorite extends Model
         return $this->belongsTo('App\User', 'user_id');
     }
     public function Product() {
-        return $this->belongsTo('App\Product', 'product_id')->select('id','title','main_image','price','description')->where('deleted',0)->where('status',1)->where('publish','Y');
+        return $this->belongsTo('App\Product', 'product_id')
+            ->select('id','title','main_image','price','description','created_at')
+            ->where('deleted',0)->where('status',1)->where('publish','Y');
     }
 }
--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -13,108 +13,111 @@ class FavoriteController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api' , ['except' => []]);
+        $this->middleware('auth:api', ['except' => []]);
     }
 
-    public function addtofavorites(Request $request){
+    public function addtofavorites(Request $request)
+    {
         $user = auth()->user();
-        if($user->active == 0){
-            $response = APIHelpers::createApiResponse(true , 406 ,  'تم حظر حسابك', 'تم حظر حسابك' , null, $request->lang );
-            return response()->json($response , 406);
+        if ($user->active == 0) {
+            $response = APIHelpers::createApiResponse(true, 406, 'تم حظر حسابك', 'تم حظر حسابك', null, $request->lang);
+            return response()->json($response, 406);
         }
-        $validator = Validator::make($request->all() , [
+        $validator = Validator::make($request->all(), [
             'product_id' => 'required',
         ]);
-        if($validator->fails()) {
-            $response = APIHelpers::createApiResponse(true , 406 ,  'بعض الحقول مفقودة', 'بعض الحقول مفقودة' , null, $request->lang );
-            return response()->json($response , 406);
+        if ($validator->fails()) {
+            $response = APIHelpers::createApiResponse(true, 406, 'بعض الحقول مفقودة', 'بعض الحقول مفقودة', null, $request->lang);
+            return response()->json($response, 406);
         }
-        $favorite = Favorite::where('product_id' , $request->product_id)->where('user_id' , $user->id)->first();
-        if($favorite){
-            $response = APIHelpers::createApiResponse(true , 406 ,  'تم إضافه هذا المنتج للمفضله من قبل', 'تم إضافه هذا المنتج للمفضله من قبل' , null, $request->lang );
-            return response()->json($response , 406);
-        }else{
+        $favorite = Favorite::where('product_id', $request->product_id)->where('user_id', $user->id)->first();
+        if ($favorite) {
+            $response = APIHelpers::createApiResponse(true, 406, 'تم إضافه هذا المنتج للمفضله من قبل', 'تم إضافه هذا المنتج للمفضله من قبل', null, $request->lang);
+            return response()->json($response, 406);
+        } else {
             $favorite = new Favorite();
             $favorite->user_id = $user->id;
             $favorite->product_id = $request->product_id;
             $favorite->save();
-            $response = APIHelpers::createApiResponse(false , 200 ,  '', '' , $favorite, $request->lang);
-            return response()->json($response , 200);
+            $response = APIHelpers::createApiResponse(false, 200, '', '', $favorite, $request->lang);
+            return response()->json($response, 200);
         }
     }
 
-    public function removefromfavorites(Request $request){
+    public function removefromfavorites(Request $request)
+    {
         $user = auth()->user();
-        if($user->active == 0){
-            $response = APIHelpers::createApiResponse(true , 406 ,  'تم حظر حسابك', 'تم حظر حسابك' , (object)[], $request->lang );
-            return response()->json($response , 406);
+        if ($user->active == 0) {
+            $response = APIHelpers::createApiResponse(true, 406, 'تم حظر حسابك', 'تم حظر حسابك', (object)[], $request->lang);
+            return response()->json($response, 406);
         }
 
-        $validator = Validator::make($request->all() , [
+        $validator = Validator::make($request->all(), [
             'product_id' => 'required',
         ]);
 
-        if($validator->fails()) {
-            $response = APIHelpers::createApiResponse(true , 406 ,  'بعض الحقول مفقودة', 'بعض الحقول مفقودة' , null, $request->lang );
-            return response()->json($response , 406);
+        if ($validator->fails()) {
+            $response = APIHelpers::createApiResponse(true, 406, 'بعض الحقول مفقودة', 'بعض الحقول مفقودة', null, $request->lang);
+            return response()->json($response, 406);
         }
 
-        $favorite = Favorite::where('product_id' , $request->product_id)->where('user_id',$user->id)->first();
-        if($favorite){
+        $favorite = Favorite::where('product_id', $request->product_id)->where('user_id', $user->id)->first();
+        if ($favorite) {
             $favorite->delete();
-            $response = APIHelpers::createApiResponse(false , 200 ,  'Deteted ', 'تم الحذف' , (object)[], $request->lang);
-            return response()->json($response , 200);
-        }else{
-            $response = APIHelpers::createApiResponse(true , 406 ,  'هذا المنتج غير موجود بالمفضله', 'هذا المنتج غير موجود بالمفضله' , null, $request->lang );
-            return response()->json($response , 406);
+            $response = APIHelpers::createApiResponse(false, 200, 'Deteted ', 'تم الحذف', (object)[], $request->lang);
+            return response()->json($response, 200);
+        } else {
+            $response = APIHelpers::createApiResponse(true, 406, 'هذا المنتج غير موجود بالمفضله', 'هذا المنتج غير موجود بالمفضله', null, $request->lang);
+            return response()->json($response, 406);
         }
     }
 
-    public function getfavorites(Request $request){
+    public function getfavorites(Request $request)
+    {
         $user = auth()->user();
-        $lang = $request->lang ;
-        if($user->active == 0){
-            $response = APIHelpers::createApiResponse(true , 406 ,  'تم حظر حسابك', 'تم حظر حسابك' , null, $request->lang );
-            return response()->json($response , 406);
-        }else {
-            $favorites = Favorite::select('id','product_id','user_id')
-                                 ->with('Product')
-                                 ->where('user_id', $user->id)
-                                 ->orderBy('id','desc')
-                                 ->get();
+        $lang = $request->lang;
+        if ($user->active == 0) {
+            $response = APIHelpers::createApiResponse(true, 406, 'تم حظر حسابك', 'تم حظر حسابك', null, $request->lang);
+            return response()->json($response, 406);
+        } else {
+            $favorites = Favorite::select('id', 'product_id', 'user_id')->has('Product')
+                ->with('Product')
+                ->where('user_id', $user->id)
+                ->orderBy('id', 'desc')
+                ->simplePaginate(12);
 
             $inc = 0;
             $data = null;
-            foreach ($favorites as $key => $row){
-                $product = Product::where('id',$row->product_id)->first();
-                if($product != null){
-                    if($product->status == 1 && $product->deleted == 0 && $product->publish == 'Y'){
+            foreach ($favorites as $key => $row) {
+                $product = Product::where('id', $row->product_id)->first();
+                if ($product != null) {
+                    if ($product->status == 1 && $product->deleted == 0 && $product->publish == 'Y') {
                         $data[$inc]['id'] = $product->id;
                         $data[$inc]['title'] = $product->title;
                         $data[$inc]['image'] = $product->main_image;
                         $data[$inc]['favorite'] = true;
 
 
-                        if ($product->price == 0 || $product->price == null ) {
+                        if ($product->price == 0 || $product->price == null) {
                             if ($lang == 'ar') {
                                 $data[$inc]['price'] = 'Ask the seller';
                             } else {
                                 $data[$inc]['price'] = 'Ask the seller';
                             }
-                        }else{
+                        } else {
                             $data[$inc]['price'] = $product->price;
                         }
 
                         $data[$inc]['created_at'] = $product->created_at;
 //                        $data[$inc]['description'] = $product->description;
-                        $inc =$inc + 1;
+                        $inc = $inc + 1;
                     }
                 }
             }
 
-            if(count($favorites) > 0) {
-                $response = APIHelpers::createApiResponse(false, 200, '', '', $data, $request->lang);
-            }else{
+            if (count($favorites) > 0) {
+                $response = APIHelpers::createApiResponse(false, 200, '', '', $favorites, $request->lang);
+            } else {
                 $response = APIHelpers::createApiResponse(false, 200, 'no item favorite to show', 'لا يوجد عناصر للعرض', null, $request->lang);
             }
             return response()->json($response, 200);
